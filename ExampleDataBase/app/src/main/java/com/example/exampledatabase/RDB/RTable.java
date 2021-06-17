04f2f7c4e0c63891f5f22b316476bd3bda0d3a0c
sandhya <sandhya.cse.gist@gmail.com>
2021-06-17 18:28:41 +0530

    added

---
+++ ExampleDataBase/app/src/main/java/com/example/exampledatabase/RDB/RTable.java
@@ -0,0 +1,48 @@
+package com.example.exampledatabase.RDB;
+
+import androidx.annotation.NonNull;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity
+
+public class RTable {
+    @NonNull
+    public String getSroll() {
+        return sroll;
+    }
+
+    public String getSname() {
+        return sname;
+    }
+
+    public String getSnumber() {
+        return snumber;
+    }
+
+    public void setSroll(@NonNull String sroll) {
+        this.sroll = sroll;
+    }
+
+    public void setSname(String sname) {
+        this.sname = sname;
+    }
+
+    public void setSnumber(String snumber) {
+        this.snumber = snumber;
+    }
+
+    public RTable(@NonNull String sroll, String sname, String snumber) {
+        this.sroll = sroll;
+        this.sname = sname;
+        this.snumber = snumber;
+    }
+
+    @NonNull
+    @PrimaryKey
+
+    String sroll;
+
+    String sname,snumber;
+
+}
