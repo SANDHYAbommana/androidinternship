bc69b5dbf81d3b2a2e6eed0d5100bdceeca01fac
sandhya <sandhya.cse.gist@gmail.com>
2021-06-22 18:50:51 +0530

    added

--- FireBaseExample/app/src/main/java/com/example/firebaseexample/MainActivity.java
+++ FireBaseExample/app/src/main/java/com/example/firebaseexample/MainActivity.java
@@ -1,16 +1,23 @@
 package com.example.firebaseexample;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
@@ -24,12 +31,23 @@ public class MainActivity extends AppCompatActivity {
         mail = findViewById(R.id.email);
         pass = findViewById(R.id.password);
         auth = FirebaseAuth.getInstance();
+        if (!isConnected()) {
+            Toast.makeText(this,"Turn on the internet",Toast.LENGTH_SHORT).show();
+        }
+        if(auth.getCurrentUser()!= null){
+            startActivity(new Intent(this,HomeActivity.class));
+            finish();
+        }
     }
 
     public void login(View view) {
         String email = mail.getText().toString().trim();
         String password = pass.getText().toString();
-        if (email.isEmpty()|password.isEmpty()){
+        if (!isConnected()) {
+            Snackbar.make(this, view, "Turn on Internet",
+                    Snackbar.LENGTH_LONG).show();
+        }
+        else if (email.isEmpty()|password.isEmpty()){
             Toast.makeText(this, "Fill all the details",
                     Toast.LENGTH_SHORT).show();
         }
@@ -41,6 +59,7 @@ public class MainActivity extends AppCompatActivity {
                             if (task.isSuccessful()){
                                 startActivity(new Intent(MainActivity.this
                                         ,HomeActivity.class));
+                                finish();
                             }
                             else{
                                 Toast.makeText(MainActivity.this, "Authentication Failed",
@@ -50,4 +69,64 @@ public class MainActivity extends AppCompatActivity {
                     });
         }
     }
+
+    public void register(View view) {
+        startActivity(new Intent(this,RegisterActivity.class));
+        finish();
+
+    }
+
+    public void reset(View view) {
+        AlertDialog.Builder b = new AlertDialog.Builder(this);
+        View v = LayoutInflater.from(this).inflate(R.layout.reset,null,false);
+        EditText email = v.findViewById(R.id.editEmailAddress);
+        b.setView(v);
+        b.setCancelable(false);
+        b.setPositiveButton("Reset", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String e = email.getText().toString();
+                if (e.isEmpty()){
+                    email.setError("Can't be Empty");
+                }else{
+                    auth.sendPasswordResetEmail(e).addOnCompleteListener(MainActivity.this,
+                            new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if (task.isSuccessful()){
+                                        Toast.makeText(MainActivity.this,
+                                                "Reset Link Sent", Toast.LENGTH_SHORT).show();
+                                    }else{
+                                        Toast.makeText(MainActivity.this, "Invalid Mail",
+                                                Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+                }
+            }
+        });
+        b.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        b.show();
+    }
+    public boolean isConnected() {
+        boolean connected = false;
+        try {
+
+
+            ConnectivityManager manager = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
+            NetworkInfo info = manager.getActiveNetworkInfo();
+            connected = (info != null) && (info.isAvailable()) && (info.isConnected());
+            return connected;
+        }
+        catch (Exception e){
+
+        }
+        return connected;
+    }
+
 }
